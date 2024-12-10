eb018b1753df82b976dd9c22bb9f6083456509c5
Luis José Sánchez <luisjose.sanchez@pragma.com.co>
2024-12-10 11:42:49 -0500

    Adding the base configuration for the challenge.

---
+++ src/main/java/com/franchise_microservice/FranchiseMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.franchise_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranchiseMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FranchiseMicroserviceApplication.class, args);
+	}
+
+}
